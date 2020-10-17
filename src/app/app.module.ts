f6e41fdfc7d6c8c5fe8dfb3ed81182b249424d33
AlexisN <alexisnarvaez97@hotmail.com>
2020-10-17 16:29:32 -0500

    Modularizando rutas y personalizado de path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { PagesModule } from './pages/pages.module';
 import { AuthModule } from './auth/auth.module';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 @NgModule({

6a1619cf3e78c5762b3790da6e2b476cbdfeea63
AlexisN <alexisnarvaez97@hotmail.com>
2020-10-17 17:15:00 -0500

    Progressbar change

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,10 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
+  styleUrls: [
+    './progress.component.css'
   ]
 })
 export class ProgressComponent implements OnInit {
+
+
+  progreso: number = 40;
+
+  get getPorcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambioValor(valor: number) {
+    
+    if(this.progreso >= 100 && valor >= 0) {
+      return this.progreso = 100;
+    }
+
+    if(this.progreso <= 0 && valor < 0) {
+      return this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor;
+
+  }
 
   constructor() { }
 
